b3e77598cd1198db5d3bc0f49d56ee02d041b531
beav <39977088+andy-beaver@users.noreply.github.com>
2024-07-23 10:04:11 -0500

    Create osquery.sh
    added osquery for test

---
+++ fragments/labels/osquery.sh
@@ -0,0 +1,8 @@
+osquery)
+    name="osquery-5.12.2"
+    type="pkg"
+    packageID="io.osquery.agent"
+    downloadURL="$(downloadURLFromGit osquery osquery)"
+    appNewVersion="$(versionFromGit osquery osquery)"
+    expectedTeamID="3522FA9PXF"
+    ;;
